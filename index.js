cdd2665f875a72b269042af697628ce685157ae4
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-24 02:07:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const list = document.querySelectorAll('.ranked-list')
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = (parseInt(list[i].innerHTML)+n).toString()
+  }
+}
+
+function deepestChild(){
+  var deepestNode = document.querySelector('#grand-node')
+  while (deepestNode.childNodes) {
+    deepestNode = deepestNode.childNodes
+  }
+  return deepestNode["1"]
+}
