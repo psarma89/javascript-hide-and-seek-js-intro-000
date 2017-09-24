36dbccbab40b4bc15748a148851b54c0ddba5dcb
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-24 02:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  var node = document.querySelector('.grand-node')
+  var node = document.querySelectorAll('.grand-node')
 
   while (node.children) {
     node = node.children
