43e43a0e1106bd4b0193e0de06c4e4a667b87192
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-24 03:10:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const list = document.querySelectorAll('.ranked-list')
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = (parseInt(list[i].innerHTML)+n).toString()
+  }
+}
+
+function deepestChild(){
+  var node = document.querySelector('#grand-node')
+
+  while (node.children[0]) {
+    node = node.children[0]
+  }
+  return node
+}
